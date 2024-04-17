b21fe399c5cc14cee206d3d18b23472b6611cf9e
Sean Baggaley <sean@csnxs.uk>
2024-04-17 13:27:09 +0100

    Server ticks are now timed and a message is printed if one took too long

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <inttypes.h>
 #include "server.h"
 #include "sockets.h"
 #include "blocks.h"
+#include "util.h"
 
 static void signal_handler(int signum);
 
@@ -33,7 +35,12 @@ int main(int argc, char *argv[]) {
 	printf("Ready!\n");
 
 	while (running) {
+		double start = get_time_s();
 		server_tick();
+		double end = get_time_s();
+		if (end - start > 1.0 / 20.0) {
+			printf("Server lagged: Tick %" PRIu64 " took too long (%f ms)\n", server.tick - 1, (end - start) * 1000.0);
+		}
 
 		usleep(1000000 / 20);
 	}
