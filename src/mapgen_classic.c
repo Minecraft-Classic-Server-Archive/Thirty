1cb497c877f380264e0ce30f049cdf08db30b5cc
Sean Baggaley <sean@csnxs.uk>
2025-12-07 01:44:21 +0000

    Fix a couple of memory leaks

--- src/mapgen_classic.c
+++ src/mapgen_classic.c
@@ -66,8 +66,6 @@ void gen_heightmap(map_t *map, genstate_t *state) {
 	combinednoise_t *noise2 = combinednoise_create(octavenoise_create(state->rng, 8), octavenoise_create(state->rng, 8));
 	octavenoise_t *noise3 = octavenoise_create(state->rng, 6);
 
-	state->heightmap = calloc(map->width * map->depth * map->height, sizeof(unsigned	int));
-
 #pragma omp parallel for
 	for (size_t x = 0; x < map->width; x++)
 	for (size_t z = 0; z < map->height; z++) {
--- src/mapsave.c
+++ src/mapsave.c
@@ -100,7 +100,7 @@ void map_save(map_t *map) {
 	}
 
 	tag_t *cpe_weather_type = nbt_create_compound("EnvWeatherType");
-	tag_t *cpe_weather_value = nbt_create_compound("WeatherType"); nbt_set_int8(cpe_weather_value, (int8_t)map->weather);
+	tag_t *cpe_weather_value = nbt_create("WeatherType"); nbt_set_int8(cpe_weather_value, (int8_t)map->weather);
 	nbt_add_tag(cpe_weather_type, cpe_weather_value);
 	nbt_add_tag(cpe_data, cpe_env_colours);
 	nbt_add_tag(cpe_data, cpe_weather_type);
